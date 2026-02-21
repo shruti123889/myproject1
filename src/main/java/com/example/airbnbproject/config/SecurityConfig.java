698965b9a777d6c765d77e4c19e2adf05a396258
Shruti Srivastava <shrutishrivastava86097@gmail.compwd>
2026-02-21 14:34:57 +0530

    fix database connection

--- src/main/java/com/example/airbnbproject/config/SecurityConfig.java
+++ src/main/java/com/example/airbnbproject/config/SecurityConfig.java
@@ -55,10 +55,10 @@ public class SecurityConfig {
     }@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
