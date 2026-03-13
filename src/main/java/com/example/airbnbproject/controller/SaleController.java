f274ddb31188ce9de5031ffc7c7ed44adeb69e61
Shruti Srivastava <shrutishrivastava86097@gmail.compwd>
2026-03-13 14:34:02 +0530

    force redeploy

--- src/main/java/com/example/airbnbproject/controller/SaleController.java
+++ src/main/java/com/example/airbnbproject/controller/SaleController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/sales")
 public class SaleController {
+    //redeploy trigger
     @Autowired
     private SaleService saleService;
     @PostMapping("/saveSale")
